8b8788edc2baf1c85b230f32f382d866118ad79d
Michael Delaney <michael@fusepilot.com>
2018-08-22 08:27:32 -0700

    fix tests

---
+++ test/test.ts
@@ -0,0 +1,24 @@
+import { renderTemplate } from "../src";
+import { tree } from "../src/serialize";
+import { remove } from "fs-extra";
+import { resolve } from "path";
+
+it("complex", async () => {
+  const outputPath = resolve(__dirname, "output-complex");
+
+  const result = await renderTemplate({
+    templatePath: resolve(__dirname, "templates", "complex"),
+    destinationPath: outputPath,
+    data: {
+      template: {
+        name: "NewProject"
+      }
+    }
+  });
+
+  const structure = await tree({ path: outputPath });
+
+  expect(structure).toMatchSnapshot();
+
+  await remove(outputPath);
+});
