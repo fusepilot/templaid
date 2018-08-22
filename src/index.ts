c448d93b9c61b2e64f3f3d8332ad6590a75e2f19
Michael Delaney <michael@fusepilot.com>
2018-08-22 08:57:08 -0700

    switch to object based api

--- src/index.ts
+++ src/index.ts
@@ -14,118 +14,157 @@ async function validateTemplatePath(templatePath: string) {
     throw Error(`template ${templatePath} does not exist.`);
 }
 
-export function getDestinationFilePath(
-  templatePath: string,
-  destinationPath: string,
-  file: string,
-  data: any
-) {
+interface GetDestinationFilePathProps {
+  templatePath: string;
+  destinationPath: string;
+  file: string;
+  data: any;
+}
+
+export function getDestinationFilePath({
+  templatePath,
+  destinationPath,
+  file,
+  data
+}: GetDestinationFilePathProps) {
   const renderedPath = getRenderedPath(file, templatePath, destinationPath);
   return getTemplatedFilePath(renderedPath, data);
 }
 
-export async function createTemplate(
-  templatePath: string,
-  templateFile: string,
-  destinationPath: string,
-  data: any
-) {
+interface CreateTemplateProps {
+  templatePath: string;
+  templateFile: string;
+  destinationPath: string;
+  data: any;
+}
+
+export async function createTemplate({
+  templatePath,
+  templateFile,
+  destinationPath,
+  data
+}: CreateTemplateProps) {
   await validateTemplatePath(templatePath);
 
   const template = await renderTemplateFile(templatePath, templateFile, data);
 
-  let newPath = getDestinationFilePath(
+  let newPath = getDestinationFilePath({
     templatePath,
     destinationPath,
-    template.fullPath,
+    file: template.fullPath,
     data
-  );
+  });
   await fs.writeFile(newPath, template.content);
 }
 
-export async function renderTemplates(
-  templatePath: string,
-  templateFilesPattern: string,
-  destinationPath: string,
-  data: any
-) {
+interface RenderTemplatesProps {
+  templatePath: string;
+  templatePattern: string;
+  destinationPath: string;
+  data: any;
+}
+
+export async function renderTemplates({
+  templatePath,
+  templatePattern,
+  destinationPath,
+  data
+}: RenderTemplatesProps) {
   await validateTemplatePath(templatePath);
 
-  const templateFiles = await search(
-    path.join(templatePath, templateFilesPattern)
-  );
+  const templateFiles = await search(path.join(templatePath, templatePattern));
 
   for (const templateFile of templateFiles) {
-    await createTemplate(templatePath, templateFile, destinationPath, data);
+    await createTemplate({ templatePath, templateFile, destinationPath, data });
   }
 }
 
-export async function renderRegularFiles(
-  templatePath: string,
-  regularFilesPattern: string,
-  destinationPath: string,
-  data: any,
-  ignorePattern?: string
-) {
+interface RenderRegularFilesProps {
+  templatePath: string;
+  regularPattern: string;
+  destinationPath: string;
+  data: any;
+  ignorePattern?: string;
+}
+
+export async function renderRegularFiles({
+  templatePath,
+  regularPattern,
+  destinationPath,
+  data,
+  ignorePattern
+}: RenderRegularFilesProps) {
   await validateTemplatePath(templatePath);
 
-  const files = await search(path.join(templatePath, regularFilesPattern), {
+  const files = await search(path.join(templatePath, regularPattern), {
     nodir: true,
     ignore: ignorePattern
   });
 
   for (const file of files) {
-    let newPath = getDestinationFilePath(
+    let newPath = getDestinationFilePath({
       templatePath,
       destinationPath,
       file,
       data
-    );
+    });
 
     await fs.copy(file, newPath);
   }
 }
 
-export async function renderFolders(
-  templatePath: string,
-  templateFoldersPattern: string,
-  destinationPath: string,
-  data: any,
-  ignorePattern?: string
-) {
+interface RenderFoldersProps {
+  templatePath: string;
+  folderPattern: string;
+  destinationPath: string;
+  data: any;
+  ignorePattern?: string;
+}
+
+export async function renderFolders({
+  templatePath,
+  folderPattern,
+  destinationPath,
+  data,
+  ignorePattern
+}: RenderFoldersProps) {
   await validateTemplatePath(templatePath);
 
-  const folders = await search(
-    path.join(templatePath, templateFoldersPattern),
-    { ignore: ignorePattern }
-  );
+  const folders = await search(path.join(templatePath, folderPattern), {
+    ignore: ignorePattern
+  });
   // const folders = await search(templateFoldersPattern, {
   //   ignore: patterns.projectTemplatePartialFiles
   // })
 
   for (const folder of folders) {
-    let newPath = getDestinationFilePath(
+    let newPath = getDestinationFilePath({
       templatePath,
       destinationPath,
-      folder,
+      file: folder,
       data
-    );
+    });
 
     await fs.ensureDir(newPath);
   }
 }
 
-export async function renderMacros(
-  templatePath: string,
-  templateMacroFilesPattern: string,
-  destinationPath: string,
-  data: any
-) {
+interface RenderMacrosProps {
+  templatePath: string;
+  macroPattern: string;
+  destinationPath: string;
+  data: any;
+}
+
+export async function renderMacros({
+  templatePath,
+  macroPattern,
+  destinationPath,
+  data
+}: RenderMacrosProps) {
   await validateTemplatePath(templatePath);
 
-  const macroPaths = await search(
-    path.join(templatePath, templateMacroFilesPattern)
-  );
+  const macroPaths = await search(path.join(templatePath, macroPattern));
 
   for (const macroPath of macroPaths) {
     //TODO handle if macro is missing or malformed
@@ -142,30 +181,51 @@ export async function renderMacros(
   }
 }
 
-export async function renderPartials(
-  templatePath: string,
-  partialPattern: string,
-  partialsPath: string,
-  destinationPath: string,
-  data: any
-) {
+interface RenderPartialsProps {
+  templatePath: string;
+  partialPattern: string;
+  partialsPath: string;
+  destinationPath: string;
+  data: any;
+}
+
+export async function renderPartials({
+  templatePath,
+  partialPattern,
+  partialsPath,
+  destinationPath,
+  data
+}: RenderPartialsProps) {
   await validateTemplatePath(templatePath);
 
   const partialPaths = await search(path.join(templatePath, partialPattern));
 
   for (const partialPath of partialPaths) {
-    let newPath = getDestinationFilePath(
+    let newPath = getDestinationFilePath({
       templatePath,
       destinationPath,
-      partialPath,
+      file: partialPath,
       data
-    ).replace(".partial", "");
+    }).replace(".partial", "");
 
     const partialName = path.basename(newPath);
     const partialTemplatePath = path.join(partialsPath, partialName);
 
     await fs.copy(partialTemplatePath, newPath);
   }
+}
+
+interface RenderTemplateProps {
+  templatePath: string;
+  destinationPath: string;
+  data?: any;
+  partialsPath?: string;
+  templatePattern?: string;
+  regularPattern?: string;
+  folderPattern?: string;
+  partialPattern?: string;
+  macroPattern?: string;
+  ignorePattern?: string;
 }
 
 export async function renderTemplate({
@@ -179,49 +239,43 @@ export async function renderTemplate({
   partialPattern = "**/*.partial",
   folderPattern = "**/",
   ignorePattern
-}: {
-  templatePath: string;
-  destinationPath: string;
-  data?: any;
-  partialsPath?: string;
-  templatePattern?: string;
-  regularPattern?: string;
-  folderPattern?: string;
-  partialPattern?: string;
-  macroPattern?: string;
-  ignorePattern?: string;
-}) {
+}: RenderTemplateProps) {
   await validateTemplatePath(templatePath);
 
   const getMeasuredTime = measureTime();
 
-  await renderFolders(
+  await renderFolders({
     templatePath,
     folderPattern,
     destinationPath,
     data,
     ignorePattern
-  );
+  });
 
   await Promise.all([
-    await renderTemplates(templatePath, templatePattern, destinationPath, data),
-    await renderRegularFiles(
+    await renderTemplates({
+      templatePath,
+      templatePattern,
+      destinationPath,
+      data
+    }),
+    await renderRegularFiles({
       templatePath,
       regularPattern,
       destinationPath,
       data,
       ignorePattern
-    ),
-    await renderPartials(
+    }),
+    await renderPartials({
       templatePath,
       partialPattern,
       partialsPath,
       destinationPath,
       data
-    )
+    })
   ]);
 
-  await renderMacros(templatePath, macroPattern, destinationPath, data);
+  await renderMacros({ templatePath, macroPattern, destinationPath, data });
 
   const createdIn = getMeasuredTime();
 
