3703796757e08ee10ee18a794313c996dd16de37
Michael Delaney <michael@fusepilot.com>
2018-10-03 11:14:42 -0700

    dont use tree to test

--- tests/test.ts
+++ tests/test.ts
@@ -1,7 +1,5 @@
 import { renderTemplate } from "../src";
-import { tree } from "../src/serialize";
 import { remove } from "fs-extra";
-import { resolve } from "path";
 import jestFs from "jest-plugin-fs";
 
 import { templates } from "../fixtures/templates";
@@ -28,91 +26,18 @@ it("can generate a complex template", async () => {
     }
   });
 
-  const output = await tree({ path: outputPath });
+  await remove("/templates");
 
-  expect(output).toMatchInlineSnapshot(`
+  expect(jestFs.files()).toMatchInlineSnapshot(`
 Object {
-  "children": Array [
-    Object {
-      "children": Array [
-        Object {
-          "content": "NewProject that should be parsed",
-          "kind": "file",
-          "name": "NewProject-a.md",
-          "path": "NewProject/NewProject-a.md",
-        },
-        Object {
-          "content": "{{template.name}} that shouldnt be parsed",
-          "kind": "file",
-          "name": "NewProject-b.md",
-          "path": "NewProject/NewProject-b.md",
-        },
-        Object {
-          "content": "file c content",
-          "kind": "file",
-          "name": "file-c.md",
-          "path": "NewProject/file-c.md",
-        },
-      ],
-      "kind": "directory",
-      "name": "NewProject",
-      "path": "NewProject",
-    },
-    Object {
-      "children": Array [
-        Object {
-          "content": "{ \\"foo\\": \\"bar\\" }",
-          "kind": "file",
-          "name": ".hidden.json",
-          "path": "folder-b/.hidden.json",
-        },
-        Object {
-          "children": Array [
-            Object {
-              "content": "file d content",
-              "kind": "file",
-              "name": "file-d.md",
-              "path": "folder-b/NewProject-folder-c/file-d.md",
-            },
-          ],
-          "kind": "directory",
-          "name": "NewProject-folder-c",
-          "path": "folder-b/NewProject-folder-c",
-        },
-        Object {
-          "children": Array [
-            Object {
-              "content": "file e content",
-              "kind": "file",
-              "name": "file-e.md",
-              "path": "folder-b/NewProject-folder-d/file-e.md",
-            },
-          ],
-          "kind": "directory",
-          "name": "NewProject-folder-d",
-          "path": "folder-b/NewProject-folder-d",
-        },
-        Object {
-          "content": "file f content",
-          "kind": "file",
-          "name": "file-f.md",
-          "path": "folder-b/file-f.md",
-        },
-      ],
-      "kind": "directory",
-      "name": "folder-b",
-      "path": "folder-b",
-    },
-    Object {
-      "children": Array [],
-      "kind": "directory",
-      "name": "folder-c",
-      "path": "folder-c",
-    },
-  ],
-  "kind": "directory",
-  "name": "complex",
-  "path": "",
+  "/output/complex/NewProject/NewProject-a.md": "NewProject that should be parsed",
+  "/output/complex/NewProject/NewProject-b.md": "{{template.name}} that shouldnt be parsed",
+  "/output/complex/NewProject/file-c.md": "file c content",
+  "/output/complex/folder-b/.hidden.json": "{ \\"foo\\": \\"bar\\" }",
+  "/output/complex/folder-b/NewProject-folder-c/file-d.md": "file d content",
+  "/output/complex/folder-b/NewProject-folder-d/file-e.md": "file e content",
+  "/output/complex/folder-b/file-f.md": "file f content",
+  "/output/complex/folder-c": null,
 }
 `);
 });
@@ -126,21 +51,11 @@ it("can generate a simple template with no folders or tokens", async () => {
     ignorePattern: "**/.gitkeep"
   });
 
-  const output = await tree({ path: outputPath });
+  await remove("/templates");
 
-  expect(output).toMatchInlineSnapshot(`
+  expect(jestFs.files()).toMatchInlineSnapshot(`
 Object {
-  "children": Array [
-    Object {
-      "content": "file content",
-      "kind": "file",
-      "name": "file.md",
-      "path": "file.md",
-    },
-  ],
-  "kind": "directory",
-  "name": "simple",
-  "path": "",
+  "/output/simple/file.md": "file content",
 }
 `);
 });
